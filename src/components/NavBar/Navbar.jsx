edb68920637a161f577e942b7bc1da6cc625acb1
shinytornado <shashankspothula@gmail.com>
2024-04-28 16:29:31 +0530

    responsiveness for mobile and tablet

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
@@ -16,10 +17,15 @@ const Navbar = () => {
     })
   }, [])
 
+  const [mobileMenu, setMobileMenu] = React.useState(false)
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+  }
+
   return (
     <nav className={`container ${sticky? 'dark-nav':''}`}>
       <img src={logo} alt="" className='logo' />
-      <ul>
+      <ul className={mobileMenu?'':'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
@@ -27,6 +33,7 @@ const Navbar = () => {
         <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
         <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
       </ul>
+      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
