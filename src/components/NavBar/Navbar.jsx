10e4d23f6fcb428bedba28743e4faf0b43f88af7
shinytornado <shashankspothula@gmail.com>
2024-04-28 15:47:57 +0530

    nav update

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
 
@@ -19,9 +20,12 @@ const Navbar = () => {
     <nav className={`container ${sticky? 'dark-nav':''}`}>
       <img src={logo} alt="" className='logo' />
       <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li><button className='btn'>Socials</button></li>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+        <li><Link to='people' smooth={true} offset={-260} duration={500}>People</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
       </ul>
     </nav>
   )
