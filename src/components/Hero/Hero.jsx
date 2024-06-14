d1dfacf3192393c80331bad2af055f7821dfed69
shinytornado <shashankspothula@gmail.com>
2024-06-14 11:23:35 +0530

    explore btn changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,25 @@ const Hero = () => {
       <div className="hero-text">
         <h1>A BETTER WORLD IS CREATED WITH BETTER MENTAL HEALTH</h1>
         <p>Our goal is to make people take better care of their mental health</p>
-        <Link to='program' smooth={true} offset={-260} duration={500} className='btn'>Explore <img src={dark_arrow} alt="" /></Link>
+        <Link to='program' smooth={true} offset={-260} duration={500}>
+        <button class="cssbuttons-io-button">
+  Explore
+  <div class="icon">
+    <svg
+      height="24"
+      width="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 0h24v24H0z" fill="none"></path>
+      <path
+        d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  </div>
+</button>        
+        </Link>
       </div>
     </div>
   )
